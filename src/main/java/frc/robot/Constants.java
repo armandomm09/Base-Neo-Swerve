04655d9860d35bfde56e55f045e447d47fd25158
Armando <97997290+armandomm09@users.noreply.github.com>
2024-01-07 22:06:31 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -78,7 +78,7 @@ public final class Constants {
                 new Translation2d(-kWheelBase / 2, kTrackWidth / 2));
 
         public static final double kMaxDriveVEL = 6.589;
-        public static final double kMaxRotVEL = 3 * (2 * Math.PI);
+        public static final double kMaxRotVEL = 3.5 * (2 * Math.PI);
 
         public static final double kDriveLimiter = kMaxDriveVEL / 4.5;
         public static final double kRotationLimiter = //
